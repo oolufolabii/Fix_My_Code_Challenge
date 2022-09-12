73d227243ebb699afda7f1939f13fc782f81fa71
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-12 09:38:14 +0100

    Fix My Code

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -7,7 +7,6 @@ import sys
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
-
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
@@ -21,7 +20,6 @@ def fizzbuzz(n):
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
-        
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
