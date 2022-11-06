3056b7f81fd42ea421405045aa03913b6cbb7e4c
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-06 07:46:14 +0100

    Fix My code challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,21 +4,21 @@
 
 class square():
     """Class to represent the square"""
-    width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
         """Init a new square"""
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """Perimeter of the square"""
-        return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """String format/representation"""
